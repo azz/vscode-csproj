335657f04ac84a398a1da490e82fb7ed0fde1a17
Lucas Azzola <Lucas.Azzola@iress.com.au>
2016-10-27 11:42:06 +1100

    Rename some functions post-refactor

--- src/statusbar.ts
+++ src/statusbar.ts
@@ -3,20 +3,20 @@ import * as vscode from 'vscode'
 let _statusBarItem: vscode.StatusBarItem
 let _statusBarItemVisible = false
 
-export function displayStatusBarItem(csprojName: string, contained = false) {
+export function displayItem(csprojName: string, contained = false) {
     _statusBarItem.text = contained ? `Contained in ${csprojName}` : `Add to ${csprojName}`
     _statusBarItem.tooltip = _statusBarItem.text
     _statusBarItem.show()
     _statusBarItemVisible = true
 }
 
-export function hideStatusBarItem() {
+export function hideItem() {
     _statusBarItem.text = ''
     _statusBarItem.hide()
     _statusBarItemVisible = false
 }
 
-export function createStatusBarItem() {
+export function createItem() {
     // Currenly only support one status bar item.
     if (_statusBarItem) {
         _statusBarItem.dispose()
@@ -29,6 +29,6 @@ export function createStatusBarItem() {
     return item
 }
 
-export function visible() {
+export function isVisible() {
     return _statusBarItemVisible
 }
